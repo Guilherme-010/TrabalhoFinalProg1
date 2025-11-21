f9eb3a34079f5104c8f56fb69cc87fc9681a3c2b
Carlos Name <guilhermealeixo1904@gmail.com>
2025-11-21 02:09:28 -0300

    meta2

--- main.c
+++ main.c
@@ -2,15 +2,8 @@
 #include "conta.h"
 #include "conta.c"
 
-int main(void){
-    transacao TransacaoAtual;
-    conta ContaAtualDoUsuario;
-    ContaAtualDoUsuario.saldo_corrente = 0;
-    ContaAtualDoUsuario.saldo_poupanca = 0;
-    int opcao = 0;
-    while (opcao != 8){
-        printf("1. Depositar (conta corrente)\n2. Sacar(Sem cheque especial)\n3. Aplicar\n4. Resgatar\n5. Fazer PIX\n6. Consultar saldos\n7.Extratos (Listar transacoes com data e hora)\n 8. Sair");
-        printf("Digite o numero da opcao que correspondente desejada.\n");
-        scanf("%d", opcao);
-    }
+int main()
+{
+    
+    return 0;
 }
